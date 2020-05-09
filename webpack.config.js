ba2c9418ec396ed649ad151af9e961441b035555
Bojan Jovanovic <bojanj6m@gmail.com>
2020-05-08 21:28:55 -0400

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  target: 'web',
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: '/node_modules/',
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['ts', 'tsx', '.js'],
+  },
+  devServer: {
+    contentBase: './',
+    port: 5000,
+  },
+};
